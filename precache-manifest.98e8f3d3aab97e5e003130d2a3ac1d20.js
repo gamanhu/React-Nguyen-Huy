e5d59ee95e976d3921ec965d2c3f8e195a559ab4
gamanhu <chrisnguyen995@gmail.com>
2020-01-04 15:55:29 +0700

    Updates

---
+++ precache-manifest.98e8f3d3aab97e5e003130d2a3ac1d20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91831819614e857186b713ebb16e815a",
+    "url": "/React-Nguyen-Huy/index.html"
+  },
+  {
+    "revision": "10a8877ce69856ad9931",
+    "url": "/React-Nguyen-Huy/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e53ce7e70c8c0b392b2e",
+    "url": "/React-Nguyen-Huy/static/css/main.c860a651.chunk.css"
+  },
+  {
+    "revision": "10a8877ce69856ad9931",
+    "url": "/React-Nguyen-Huy/static/js/2.00d1d017.chunk.js"
+  },
+  {
+    "revision": "6af4b795fbb399f1f705df5ce8ef8b34",
+    "url": "/React-Nguyen-Huy/static/js/2.00d1d017.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e53ce7e70c8c0b392b2e",
+    "url": "/React-Nguyen-Huy/static/js/main.aa307abb.chunk.js"
+  },
+  {
+    "revision": "b6a36deebac4f50a901f",
+    "url": "/React-Nguyen-Huy/static/js/runtime-main.4818e357.js"
+  }
+]);
